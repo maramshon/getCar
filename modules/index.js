c86b6d6dd92ee826a39865092116061d464343a7
maramshon <mralshon@yahoo.com>
2017-09-30 14:06:18 +0300

    singUp page but not finished yet

---
+++ modules/index.js
@@ -0,0 +1,20 @@
+var SignUpApp = angular.module('SignUpApp',[]);
+
+// SignUpApp.config(function(){
+
+
+
+// });
+
+// SignUpApp.run(function(){
+
+
+
+// })
+
+SignUpApp.controller("signUpController",  function($scope){
+
+	$scope.x="maram";
+
+});
+
